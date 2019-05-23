403816322bc8e62a1dd0c88e01dc6ceb3712ad00
Piyush Pushkar <Piyush.Pushkar@mu-sigma.com>
2019-05-23 12:02:40 +0530

    Added alternate implementation of merge sort

--- sorting-algos.js
+++ sorting-algos.js
@@ -50,22 +50,21 @@ function selectionSort(arrayToBeSorted) {
 }
 
 
-function mergeSort(arrayToBeSorted) {
+function mergeSort1(arrayToBeSorted) {
     return (function divide(dividedArray) {
         const n = dividedArray.length;
         if (n > 1) {
-            return merge(divide(dividedArray.slice(0, n / 2)), divide(dividedArray.slice(n / 2)));
+            return merge1(divide(dividedArray.slice(0, n / 2)), divide(dividedArray.slice(n / 2)));
         } else {
             return dividedArray;
         }
     })(arrayToBeSorted);
 }
 
-function merge(array1, array2) {
+function merge1(array1, array2) {
     const mergedArray = [];
     let pointer1 = 0;
     let pointer2 = 0;
-    console.log('COmparing arrays', array1, array2);
     while (pointer1 < array1.length || pointer2 < array2.length) {
         const ele1 = array1[pointer1];
         const ele2 = array2[pointer2];
@@ -80,7 +79,42 @@ function merge(array1, array2) {
     return mergedArray;
 }
 
-// console.log(selectionSort([50, 200, 40, 60, 10, 30]));
-// console.log(binarySum("1011", "111"));
-// console.log(merge([1, 2, 9], [3, 4, 4]));
-console.log(mergeSort1([5,3,8,2]));
+function mergeSortFull(arrayToBeSorted) {
+    (function mergeSort(startIndex, endIndex) {
+        const n = endIndex - startIndex;
+        if (n < 2) {
+            return;
+        }
+        const midIndex = Math.round((startIndex + endIndex) / 2);
+        mergeSort(startIndex, midIndex);
+        mergeSort(midIndex, endIndex);
+        merge(arrayToBeSorted, startIndex, midIndex, endIndex);
+    })(0, arrayToBeSorted.length);
+    return arrayToBeSorted;
+}
+
+function merge(arrayToBeSorted, startIndex, midIndex, endIndex) {
+    let pointerL = 0;
+    let pointerR = 0;
+    const arrayL = arrayToBeSorted.slice(startIndex, midIndex);
+    const arrayR = arrayToBeSorted.slice(midIndex, endIndex);
+    while (pointerL < arrayL.length || pointerR < arrayR.length) {
+        const ele1 = arrayL[pointerL];
+        const ele2 = arrayR[pointerR];
+        if (ele1 < ele2 || !ele2) {
+            arrayToBeSorted[startIndex + pointerL++ + pointerR] = ele1;
+        } else {
+            arrayToBeSorted[startIndex + pointerR++ + pointerL] = ele2;
+        }
+    }
+    arrayL.length = arrayR.length = 0;
+    return arrayToBeSorted;
+}
+
+module.exports = {
+    selectionSort,
+    insertionSort,
+    mergeSort1,
+    mergeSortFull
+}
+
